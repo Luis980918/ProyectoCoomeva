174d18a9bc44c5952c3d73c2c191a9e503a34b6d
Luis980918 <55850466+Luis980918@users.noreply.github.com>
2020-04-18 21:25:45 -0500

    Revert "Revert "Se corrigen errores""
    This reverts commit 6b7e273588a6e5e321cfb02a4b2eda22af69eb2b.

--- src/main/java/com/udea/analisis/rtf/repository/CitaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.udea.analisis.rtf.repository;
-
-import com.udea.analisis.rtf.entity.Cita;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CitaRepository extends CrudRepository<Cita, Long> {
-}
